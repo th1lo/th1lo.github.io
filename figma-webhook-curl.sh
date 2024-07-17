3788d77e39a22a0f2d2317607a90bf674d7f98ca
th1lo <mail@th1lo.com>
2024-07-17 13:49:21 +0200

    New start

--- figma-webhook-curl.sh
+++ figma-webhook-curl.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
+
 curl -X POST \
-  -H 'X-FIGMA-TOKEN: figd_tlrQW6Bhq2x_ASkbQFiJqxEgJk5NjKdpHo8Xdbcg' \
+  -H "X-FIGMA-TOKEN: $FIGMA_PERSONAL_ACCESS_TOKEN" \
   -H 'Content-Type: application/json' \
   -d '{
     "event_type": "LIBRARY_PUBLISH",
+<<<<<<< HEAD
     "team_id": "YOUR_TEAM_ID",
     "endpoint": "https://your-server-url.com/webhook",
     "passcode": "YOUR_SECRET_PASSCODE",
+=======
+    "team_id": "'$FIGMA_TEAM_ID'",
+    "endpoint": "https://api.github.com/repos/'$GITHUB_USERNAME'/'$GITHUB_REPO_NAME'/dispatches",
+    "passcode": "'$GITHUB_PERSONAL_ACCESS_TOKEN'",
+>>>>>>> be9eff5 (Remove sensitive information)
     "description": "Design System Library Publish Notifications"
   }' \
   'https://api.figma.com/v2/webhooks'
