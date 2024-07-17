64a814fda3d158cba178b38c550ee1586da5753a
th1lo <mail@th1lo.com>
2024-07-17 21:39:24 +0200

    Updated Action & Hook

--- figma-webhook-curl.sh
+++ figma-webhook-curl.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-
-# Load environment variables
-source .env
-
 curl -X PUT \
   -H "X-FIGMA-TOKEN: $FIGMA_TOKEN" \
   -H "Content-Type: application/json" \
@@ -11,6 +6,10 @@ curl -X PUT \
     "team_id": "'$FIGMA_TEAM_ID'",
     "endpoint": "https://api.github.com/repos/'$GH_USERNAME'/'$GH_REPO_NAME'/dispatches",
     "passcode": "'$WEBHOOK_PASSCODE'",
-    "description": "Design System Library Publish Notifications"
+    "description": "Design System Library Publish Notifications",
+    "client_payload": {
+      "event_type": "figma-webhook",
+      "passcode": "'$WEBHOOK_PASSCODE'"
+    }
   }' \
-  "https://api.figma.com/v2/webhooks/$WEBHOOK_ID"
+  "https://api.figma.com/v2/webhooks/758750"
